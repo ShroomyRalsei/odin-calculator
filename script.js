13b0193f2db300520b87d563f933657803f70e5f
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-20 14:55:27 -0400

    feat:Add query selectors for btns and display txt

---
+++ script.js
@@ -0,0 +1,37 @@
+const acBtn = document.querySelector("#ac-btn");
+
+const plusMinusBtn = document.querySelector("#plus-minus-btn");
+
+const slashBtn = document.querySelector("#slash-btn");
+
+const sevenBtn = document.querySelector("#seven-btn");
+
+const eightBtn = document.querySelector("#eight-btn");
+
+const nineBtn = document.querySelector("#nine-btn");
+
+const multiplicationBtn = document.querySelector("#multiplication-btn");
+
+const fourBtn = document.querySelector("#four-btn");
+
+const fiveBtn = document.querySelector("#five-btn");
+
+const sixBtn = document.querySelector("#six-btn");
+
+const minusBtn = document.querySelector("#minus-btn");
+
+const oneBtn = document.querySelector("#one-btn");
+
+const twoBtn = document.querySelector("#two-btn");
+
+const threeBtn = document.querySelector("#three-btn");
+
+const plusBtn = document.querySelector("#plus-btn");
+
+const zeroBtn = document.querySelector("#zero-btn");
+
+const decimalBtn = document.querySelector("#decimal-btn");
+
+const equalsBtn = document.querySelector("#equals-btn");
+
+const displayText = document.querySelector("#display-text");
